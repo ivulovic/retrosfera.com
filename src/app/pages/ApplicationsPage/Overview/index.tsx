562e0cb7e46088e49ea7ddc785b50443cc3e39ef
ivulovic <ivn.vulovic@gmail.com>
2021-11-25 23:07:29 +0100

    added preview image

--- src/app/pages/ApplicationsPage/Overview/index.tsx
+++ src/app/pages/ApplicationsPage/Overview/index.tsx
@@ -4,7 +4,8 @@ import Title from 'app/components/Title';
 import { useTranslation } from 'react-i18next';
 import { useRouteMatch } from 'react-router';
 import wishlistComputer from 'app/images/wishlist-preview.png';
-import img from 'app/images/demoapp.png';
+import cryptoexchangeComputer from 'app/images/cryptoexchange-preview.png';
+// import img from 'app/images/demoapp.png';
 import { useSelector } from 'react-redux';
 import { makeSelectIsUserAuthenticated } from 'app/providers/AuthProvider/selectors';
 
@@ -26,7 +27,7 @@ export default function ApplicationOverview(): JSX.Element {
       id: 'cryptoexchange',
       title: t('cryptoexchangeTitle'),
       description: t('cryptoexchangeDescription'),
-      img: img,
+      img: cryptoexchangeComputer,
       url: `${url}/cryptoexchange`,
       link: t(linkLabel),
     },
